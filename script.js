35c5afc5cc84b9146369a18d036479ea9f5fc1c5
김동훈 <112303dh@naver.com>
2020-08-14 18:20:32 +0900

    0.1

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 window.document.oncontextmenu = new Function("return false");
 window.document.onselectstart = new Function("return false");
 window.document.ondragstart = new Function("return false");
-var selectIndex=0;
 
-document.body.onkeydown = function (e) {
+//배경이미지
+document.body.style.backgroundImage = "url('../model/f1.png')";
+
+//해당인덱스
+var tIndex = 0;
+//키입력
+window.document.body.onkeydown = function (e) {
     try {
         // 이벤트를 정규화한뒤 기본 동작을 막는다.
         e = e || window.event;
@@ -14,16 +19,37 @@ document.body.onkeydown = function (e) {
         else {
             e.returnValue = false;
         }
+        //어떤키인지 구분
+        switch (e.keyCode) {
+            case 87:
+            case 38:
+                document.title = "상";
+                tIndex = (tIndex - 2 + 8) % 8;
+                break;
+            case 83:
+            case 40:
+                document.title = "하";
+                tIndex = (tIndex + 2 + 8) % 8;
+                break;
+            case 65:
+            case 37:
+                document.title = "좌";
+                tIndex = (tIndex - 1 + 8) % 8;
+                break;
+            case 68:
+            case 39:
+                document.title = "우";
+                tIndex = (tIndex + 1 + 8) % 8;
+                break;
+            case 13:
+                document.title = "선택";
+                break;
+            case 9:
+                document.title = "확인";
+                break;
+        }
 
-        //상:87(w),38
-        //하:83(s),40
-        //좌:65(a),37
-        //우:68(d),39
-        //선택:13(enter)
-        //확인:9(tab)
-        document.body.style.backgroundImage="url('../model/f1.png')";
-        
-
+        document.title += "" + tIndex;
     }
     catch (catchID) {
         alert("펑");
