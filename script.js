d3c3f69be12f242d2ca100faa21da68af0ef8f81
김동훈 <112303dh@naver.com>
2020-08-14 16:33:16 +0900

    0.0

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ document.body.onkeydown = function (e) {
         //우:68(d),39
         //선택:13(enter)
         //확인:9(tab)
-document.body.style.backgroundImage="model/f1.png";
+        document.getElementsByName("body").style.backgroundImage="model/f1.png";
 
     }
     catch (catchID) {
